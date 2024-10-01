189cea5dd0f87c6957d4ae7f42f0f327aed65268
SebastianFabio <seabass.fabio@gmail.com>
2024-10-01 15:00:38 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,33 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    return render_template('response.html')
+    genre = request.args['genre']
+    age = request.args['age']
+    if genre == 'Action' and age == 'older':
+        reply = "You should watch Indiana Jones"
+        movimage = "/static/Indiana.jpg"
+    elif genre == 'Action' and age == 'newer':
+        reply = "You should watch Borderlands"
+        movimage = "/static/Borderlands.jpg"
+    elif genre == 'Horror' and age == 'older':
+        reply = "You should watch Halloween"
+        movimage = "/static/Halloween.jpg"
+    elif genre == 'Horror' and age == 'newer':
+        reply = "You should watch Nope"
+        movimage = "/static/Nope.jpg"
+    elif genre == 'Sci-Fi' and age == 'older':
+        reply = "You should watch Alien"
+        movimage = "/static/Alien.jpg"
+    elif genre == 'Sci-Fi' and age == 'newer':
+        reply = "You should watch Interstellar"
+        movimage = "/static/Interstellar.jpg"
+    elif genre == 'Fantasy' and age == 'older':
+        reply = "You should watch Lord of the Rings"
+        movimage = "/static/LOTR.jpg"
+    else:
+        reply = "You should watch Dune"
+        movimage = "/static/Dune.jpg"
+    return render_template('response.html', response = reply, image = movimage)
 
 if __name__=="__main__":
     app.run(debug=True)
